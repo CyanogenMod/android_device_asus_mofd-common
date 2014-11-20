097466e6a064995e3879e199fafb477a18c992b5
Elliott Hughes <enh@google.com>
2014-11-20 13:12:25 -0800

    mofd-common: cherry-pick of: Remove asus/fugu no-op.
    Change-Id: I25a2f3d02a31be572b1ddb30faf857c8e1219c99
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,6 @@ TARGET_CPU_ABI := x86
 TARGET_CPU_ABI2 := armeabi-v7a
 TARGET_CPU_ABI_LIST := x86,armeabi-v7a,armeabi
 TARGET_CPU_ABI_LIST_32_BIT := x86,armeabi-v7a,armeabi
-TARGET_CPU_SMP := true
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
 TARGET_BOARD_PLATFORM := moorefield
 TARGET_BOOTLOADER_BOARD_NAME := moorefield
