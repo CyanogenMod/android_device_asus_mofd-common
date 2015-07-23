2db5d328464a408c35f6cf9da8eea1fc743221e5
Christopher R. Palmer <crpalmer@gmail.com>
2015-07-22 21:02:54 -0400

    camera: Add a dummy wrapper HAL
    Change-Id: Ibdc3c5a32493e17e489b54077844eb6c593e2c56
    
    Conflicts:
    	proprietary-files.txt
    
    Conflicts:
    	proprietary-files.txt

--- mofd.mk
+++ mofd.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
     charger \
     charger_res_images
 
+# Camera
+PRODUCT_PACKAGES += \
+    camera.mofd_v1 \
+
 # GPS
 PRODUCT_COPY_FILES += \
     device/asus/mofd-common/configs/gps.conf:system/etc/gps.conf \
