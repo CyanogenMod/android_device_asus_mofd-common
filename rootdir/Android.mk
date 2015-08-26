0219ecd3fb7275e70ef2d1792cde6cf5dcb3b633
Christopher R. Palmer <crpalmer@gmail.com>
2015-08-26 09:18:04 -0400

    mofd-common: Restore asus' init.zram.rc
    Change-Id: I63f3de3d1c991fb0369b7b5f283b9e3d1f49b7b3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -185,6 +185,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.zram.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.zram.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := intel_prop.cfg
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
