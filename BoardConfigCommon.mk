5d7f33404af63cce15e4bf10eef18087538c0419
Christopher R. Palmer <crpalmer@gmail.com>
2015-09-18 06:28:51 -0400

    mofd-common: Sort and organize device / board files
    Change-Id: I2e474805be82c4a9444d9c49eac4f9e704827c33

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -28,47 +28,36 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 TARGET_SPECIFIC_HEADER_PATH := device/asus/mofd-common/include
 
-# Partitions
-BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2415919104
-BOARD_FLASH_BLOCK_SIZE := 2048
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-
 TARGET_DROIDBOOT_LIBS := libintel_droidboot
 
 # Adb
 BOARD_FUNCTIONFS_HAS_SS_COUNT := true
 
-# Init
-TARGET_IGNORE_RO_BOOT_SERIALNO := true
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-TARGET_RECOVERY_FSTAB := device/asus/mofd-common/rootdir/etc/fstab.mofd_v1
-
-# Bootloader
-TARGET_OTA_ASSERT_DEVICE := Z00A,Z008
-
-# Hardware
-BOARD_HARDWARE_CLASS := device/asus/mofd-common/cmhw
-
-# Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/asus/moorefield
-TARGET_KERNEL_ARCH := x86_64
-BOARD_KERNEL_IMAGE_NAME := bzImage
-TARGET_KERNEL_CONFIG := cyanogenmod_zenfone2_defconfig
-
-# Kernel cmdline
-BOARD_KERNEL_CMDLINE := init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 n_gsm.mux_base_conf=\"ttyACM0,0 ttyXMM0,1\"
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_TINY_ALSA_AUDIO := true
 
 # Binder API version
 TARGET_USES_64_BIT_BINDER := true
 
-# Enable dex-preoptimization to speed up first boot sequence
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/mofd-common/bluetooth
+
+# Bootloader
+TARGET_OTA_ASSERT_DEVICE := Z00A,Z008
+# bootstub as 2nd bootloader
+TARGET_BOOTLOADER_IS_2ND := true
+
+# Camera
+INTEL_USE_CAMERA_UVC := true
+INTEL_VIDEO_XPROC_SHARING := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+
+# Dex-preoptimization: Speeds up initial boot (if we ever o a user build, which we don't)
 ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),user)
     ifeq ($(WITH_DEXPREOPT),)
@@ -77,24 +66,11 @@ ifeq ($(HOST_OS),linux)
   endif
 endif
 
-# Security
-BUILD_WITH_SECURITY_FRAMEWORK := chaabi_token
-BUILD_WITH_CHAABI_SUPPORT := true
+# Hardware
+BOARD_HARDWARE_CLASS := device/asus/mofd-common/cmhw
 
-# Wifi
-BOARD_WLAN_DEVICE           := bcmdhd
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WPA_SUPPLICANT_VERSION      := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_HOSTAPD_DRIVER        := NL80211
-WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcmdhd.bin"
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/asus/mofd-common/bluetooth
+# Houdini: enable ARM codegen for x86
+BUILD_ARM_FOR_X86 := true
 
 # IMG graphics
 BOARD_GFX_REV := RGX6400
@@ -104,11 +80,7 @@ INTEL_HWC_MOOREFIELD := true
 HWUI_IMG_FBO_CACHE_OPTIM := true
 TARGET_INTEL_HWCOMPOSER_FORCE_ONLY_ONE_RGB_LAYER := true
 
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-BOARD_USES_TINY_ALSA_AUDIO := true
-
-# System's VSYNC phase offsets in nanoseconds
+# IMG Graphics: System's VSYNC phase offsets in nanoseconds
 VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
 SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 
@@ -133,11 +105,35 @@ MAX_EGL_CACHE_SIZE := 1048576
 INTEL_VA := true
 BUILD_WITH_FULL_STAGEFRIGHT := true
 BOARD_USES_VIDEO := true
+
+# Disable IMG RS GPU driver
+# OVERRIDE_RS_DRIVER := libPVRRS.so
+
+# enabled to carry out all drawing operations performed on a View's canvas with GPU for 2D rendering pipeline.
+USE_OPENGL_RENDERER := true
+
+# Init
+TARGET_IGNORE_RO_BOOT_SERIALNO := true
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/asus/moorefield
+TARGET_KERNEL_ARCH := x86_64
+BOARD_KERNEL_IMAGE_NAME := bzImage
+TARGET_KERNEL_CONFIG := cyanogenmod_zenfone2_defconfig
+
+# Kernel cmdline
+BOARD_KERNEL_CMDLINE := init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 n_gsm.mux_base_conf=\"ttyACM0,0 ttyXMM0,1\"
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Media
 BOARD_USES_WRS_OMXIL_CORE := true
 BOARD_USES_MRST_OMX := true
 USE_HW_VP8 := true
 
-# DRM Protected Video
+# Media: DRM Protected Video
 BOARD_WIDEVINE_OEMCRYPTO_LEVEL := 1
 USE_INTEL_SECURE_AVC := true
 
@@ -155,18 +151,28 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 
 COMMON_GLOBAL_CFLAGS += -DGFX_BUF_EXT
 
-# Disable IMG RS GPU driver
-# OVERRIDE_RS_DRIVER := libPVRRS.so
+# Partitions
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2415919104
+BOARD_FLASH_BLOCK_SIZE := 2048
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 
-# enable ARM codegen for x86 with Houdini
-BUILD_ARM_FOR_X86 := true
+# Radio
+BOARD_RIL_SUPPORTS_MULTIPLE_CLIENTS := true
+BOARD_RIL_CLASS := ../../../device/asus/mofd-common/ril
 
-# enabled to carry out all drawing operations performed on a View's canvas with GPU for 2D rendering pipeline.
-USE_OPENGL_RENDERER := true
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+TARGET_RECOVERY_FSTAB := device/asus/mofd-common/rootdir/etc/fstab.mofd_v1
 
-# bootstub as 2nd bootloader
-TARGET_BOOTLOADER_IS_2ND := true
+# Security
+BUILD_WITH_SECURITY_FRAMEWORK := chaabi_token
+BUILD_WITH_CHAABI_SUPPORT := true
 
+# SELinux
 BOARD_SEPOLICY_DIRS := device/asus/mofd-common/sepolicy
 BOARD_SEPOLICY_UNION := \
     bd_prov.te \
@@ -193,20 +199,16 @@ BOARD_SEPOLICY_UNION := \
     property_contexts \
     service_contexts
 
-# Camera
-INTEL_USE_CAMERA_UVC := true
-INTEL_VIDEO_XPROC_SHARING := true
-
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
-
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
-# Radio
-BOARD_RIL_SUPPORTS_MULTIPLE_CLIENTS := true
-BOARD_RIL_CLASS := ../../../device/asus/mofd-common/ril
+# Wifi
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER        := NL80211
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP      := "/system/etc/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/system/etc/firmware/fw_bcmdhd.bin"
 
 # Use the non-open-source parts, if they're present
 -include vendor/asus/mofd-common/BoardConfigVendor.mk
--- mofd.mk
+++ mofd.mk
@@ -24,6 +24,143 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui
 DEVICE_PACKAGE_OVERLAYS := \
     device/asus/mofd-common/overlay
 
+# Audio
+PRODUCT_PACKAGES += \
+    libtinycompress \
+    libtinyalsa \
+    audio.a2dp.default \
+    audio.r_submix.default \
+    audio.usb.default
+
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/audio/asound.conf:system/etc/asound.conf \
+    device/asus/mofd-common/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/asus/mofd-common/audio/route_criteria.conf:system/etc/route_criteria.conf
+
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    bt.hfp.WideBandSpeechEnabled=true
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger \
+    charger_res_images
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.dalvik.vm.isa.arm=x86 \
+    dalvik.vm.implicit_checks=none
+
+# Display
+PRODUCT_PACKAGES += \
+    pvr_drv_video
+
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/powervr.ini:system/etc/powervr.ini
+
+# GPS
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/configs/gps.conf:system/etc/gps.conf \
+    device/asus/mofd-common/configs/gps.xml:system/etc/gps.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.spid.gps.tty=ttyMFD2
+
+# Houdini (arm native bridge)
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.enable.native.bridge.exec=1 \
+
+ADDITIONAL_DEFAULT_PROPERTIES += ro.dalvik.vm.native.bridge=libhoudini.so
+
+# Keyhandler
+PRODUCT_PACKAGES += \
+    com.cyanogenmod.keyhandler
+
+# Key layout files
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/keylayout/ASUS_TransKeyboard.kl:system/usr/keylayout/ASUS_TransKeyboard.kl \
+    device/asus/mofd-common/keylayout/ftxxxx_ts.kl:system/usr/keylayout/ftxxxx_ts.kl \
+    device/asus/mofd-common/keylayout/ftxxxx_ts.kcm:system/usr/keychars/ftxxxx_ts.kcm \
+    device/asus/mofd-common/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    device/asus/mofd-common/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl:system/usr/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl \
+    device/asus/mofd-common/keylayout/Vendor_0b05_Product_17fc.kl:system/usr/keylayout/Vendor_0b05_Product_17fc.kl \
+    device/asus/mofd-common/keylayout/Vendor_0b05_Product_1803.kl:system/usr/keylayout/Vendor_0b05_Product_1803.kl
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.mofd_v1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    drm.service.enabled=true \
+    ro.com.widevine.cachesize=16777216 \
+    media.stagefright.cache-params=10240/20480/15 \
+    media.aac_51_output_enabled=true \
+
+PRODUCT_COPY_FILES += \
+    device/asus/mofd-common/media/media_profiles.xml:system/etc/media_profiles.xml \
+    device/asus/mofd-common/media/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    device/asus/mofd-common/media/media_codecs.xml:system/etc/media_codecs.xml \
+    device/asus/mofd-common/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf \
+    device/asus/mofd-common/media/video_isv_profile.xml:system/etc/video_isv_profile.xml
+
+# Media: SDK and OMX IL components
+PRODUCT_PACKAGES += \
+    msvdx_bin \
+    topaz_bin
+
+# Media: libva
+PRODUCT_PACKAGES += \
+    libva \
+    libva-android \
+    libva-tpi \
+    vainfo
+
+# Media: libstagefrighthw
+PRODUCT_PACKAGES += \
+    libstagefrighthw
+
+# Media: libmix
+PRODUCT_PACKAGES += \
+    libmixvbp_mpeg4 \
+    libmixvbp_h264 \
+    libmixvbp_h264secure \
+    libmixvbp_vc1 \
+    libmixvbp_vp8 \
+    libva_videodecoder \
+    libva_videoencoder
+
+PRODUCT_PACKAGES += \
+    libwrs_omxil_common \
+    libwrs_omxil_core_pvwrapped \
+    libOMXVideoDecoderAVC \
+    libOMXVideoDecoderH263 \
+    libOMXVideoDecoderMPEG4 \
+    libOMXVideoDecoderWMV \
+    libOMXVideoDecoderVP8 \
+    libOMXVideoDecoderVP9HWR \
+    libOMXVideoDecoderVP9Hybrid \
+    libOMXVideoEncoderAVC \
+    libOMXVideoEncoderH263 \
+    libOMXVideoEncoderMPEG4 \
+    libOMXVideoEncoderVP8
+
+# Media: libISV
+PRODUCT_PACKAGES += \
+    libisv_omx_core
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.ril.status.polling.enable=0 \
+    rild.libpath=/system/lib/librapid-ril-core.so \
+    ro.telephony.default_network=9 \
+    ro.telephony.ril_class=Zenfone2RIL
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     config_init.sh \
@@ -56,130 +193,21 @@ PRODUCT_PACKAGES += \
     thermald \
     ueventd.mofd_v1.rc
 
-# Audio
+# Thermal itux
+ENABLE_ITUXD := true
 PRODUCT_PACKAGES += \
-    libtinycompress \
-    libtinyalsa \
-    audio.a2dp.default \
-    audio.r_submix.default \
-    audio.usb.default
+    ituxd
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
-
-# GPS
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/configs/gps.conf:system/etc/gps.conf \
-    device/asus/mofd-common/configs/gps.xml:system/etc/gps.xml
-
+# USB
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.spid.gps.tty=ttyMFD2
+    persist.sys.isUsbOtgEnabled=1
 
-# Lights
-PRODUCT_PACKAGES += \
-    lights.mofd_v1
-
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ril.status.polling.enable=0 \
-    rild.libpath=/system/lib/librapid-ril-core.so \
-    ro.telephony.default_network=9 \
-    ro.telephony.ril_class=Zenfone2RIL
-
-# Audio
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/audio/asound.conf:system/etc/asound.conf \
-    device/asus/mofd-common/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    device/asus/mofd-common/audio/route_criteria.conf:system/etc/route_criteria.conf
-
-# Add props used in stock
-PRODUCT_PROPERTY_OVERRIDES +=  \
-    ro.dalvik.vm.isa.arm=x86 \
-    ro.enable.native.bridge.exec=1 \
-    drm.service.enabled=true \
-    ro.com.widevine.cachesize=16777216 \
-    media.stagefright.cache-params=10240/20480/15 \
-    media.aac_51_output_enabled=true \
-    dalvik.vm.implicit_checks=none
-
-# Set the prop to enable arm native bridge
-ADDITIONAL_DEFAULT_PROPERTIES += ro.dalvik.vm.native.bridge=libhoudini.so
-
-# set USB OTG enabled to add support for USB storage type
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.isUsbOtgEnabled=1
-
-# Set default USB interface
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += persist.sys.usb.config=mtp
-
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/bluetooth/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    bt.hfp.WideBandSpeechEnabled=true
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
 
 # IMG graphics
 PRODUCT_PACKAGES += \
     hwcomposer.moorefield
-
-#Video
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/media/media_profiles.xml:system/etc/media_profiles.xml \
-    device/asus/mofd-common/media/wrs_omxil_components.list:system/etc/wrs_omxil_components.list \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    device/asus/mofd-common/media/media_codecs.xml:system/etc/media_codecs.xml \
-    device/asus/mofd-common/media/mfx_omxil_core.conf:system/etc/mfx_omxil_core.conf \
-    device/asus/mofd-common/media/video_isv_profile.xml:system/etc/video_isv_profile.xml
-
-# psb video
-PRODUCT_PACKAGES += \
-    pvr_drv_video
-
-# Media SDK and OMX IL components
-PRODUCT_PACKAGES += \
-    msvdx_bin \
-    topaz_bin
-
-# libva
-PRODUCT_PACKAGES += \
-    libva \
-    libva-android \
-    libva-tpi \
-    vainfo
-
-#libstagefrighthw
-PRODUCT_PACKAGES += \
-    libstagefrighthw
-
-# libmix
-PRODUCT_PACKAGES += \
-    libmixvbp_mpeg4 \
-    libmixvbp_h264 \
-    libmixvbp_h264secure \
-    libmixvbp_vc1 \
-    libmixvbp_vp8 \
-    libva_videodecoder \
-    libva_videoencoder
-
-PRODUCT_PACKAGES += \
-    libwrs_omxil_common \
-    libwrs_omxil_core_pvwrapped \
-    libOMXVideoDecoderAVC \
-    libOMXVideoDecoderH263 \
-    libOMXVideoDecoderMPEG4 \
-    libOMXVideoDecoderWMV \
-    libOMXVideoDecoderVP8 \
-    libOMXVideoDecoderVP9HWR \
-    libOMXVideoDecoderVP9Hybrid \
-    libOMXVideoEncoderAVC \
-    libOMXVideoEncoderH263 \
-    libOMXVideoEncoderMPEG4 \
-    libOMXVideoEncoderVP8
-
-#libISV
-PRODUCT_PACKAGES += libisv_omx_core
 
 # pvr
 PRODUCT_PACKAGES += \
@@ -191,15 +219,7 @@ PRODUCT_PACKAGES += \
     dristat \
     drmstat
 
-# Wifi
-PRODUCT_PACKAGES += \
-    libwpa_client \
-    lib_driver_cmd_bcmdhd \
-    hostapd \
-    dhcpcd.conf \
-    wpa_supplicant \
-    wpa_supplicant.conf
-
+# Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -224,28 +244,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
 
-# Key layout files
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/keylayout/ASUS_TransKeyboard.kl:system/usr/keylayout/ASUS_TransKeyboard.kl \
-    device/asus/mofd-common/keylayout/ftxxxx_ts.kl:system/usr/keylayout/ftxxxx_ts.kl \
-    device/asus/mofd-common/keylayout/ftxxxx_ts.kcm:system/usr/keychars/ftxxxx_ts.kcm \
-    device/asus/mofd-common/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    device/asus/mofd-common/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl:system/usr/keylayout/lm49453-audio_Intel_MID_Audio_Jack.kl \
-    device/asus/mofd-common/keylayout/Vendor_0b05_Product_17fc.kl:system/usr/keylayout/Vendor_0b05_Product_17fc.kl \
-    device/asus/mofd-common/keylayout/Vendor_0b05_Product_1803.kl:system/usr/keylayout/Vendor_0b05_Product_1803.kl
-
-# Keyhandler
+# Wifi
 PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
-#GFX Config
-PRODUCT_COPY_FILES += \
-    device/asus/mofd-common/powervr.ini:system/etc/powervr.ini
-
-# Thermal itux
-ENABLE_ITUXD := true
-PRODUCT_PACKAGES += \
-    ituxd
+    libwpa_client \
+    lib_driver_cmd_bcmdhd \
+    hostapd \
+    dhcpcd.conf \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 $(call inherit-product-if-exists, vendor/asus/mofd-common/mofd-common-vendor.mk)
 
