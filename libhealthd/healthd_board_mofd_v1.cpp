bd2d787714bd281f37e0d9b94bca7fc354b5b8bb
Christopher R. Palmer <crpalmer@gmail.com>
2015-09-29 07:52:04 -0400

    libhealthd: Update to the new naming convention for the fast charging
    Change-Id: I0bd1e8b94af3f4f40c0c84824e61fa9536b4ca39

--- libhealthd/healthd_board_mofd_v1.cpp
+++ libhealthd/healthd_board_mofd_v1.cpp
@@ -26,19 +26,19 @@ static int mapBatteryStatusString(const char *status)
     return HEALTHD_MAP_CONTINUE_SEARCH;
 }
 
-static int mapChargeTypeString(const char *charge_type)
+static int mapChargeRateString(const char *charge_rate)
 {
-    if (strcmp(charge_type, "Quick charging") == 0)
-        return android::BATTERY_CHARGE_TYPE_FAST_CHARGING;
+    if (strcmp(charge_rate, "Quick charging") == 0)
+        return android::BATTERY_CHARGE_RATE_FAST_CHARGING;
     else
-        return android::BATTERY_CHARGE_TYPE_UNKNOWN;
+        return android::BATTERY_CHARGE_RATE_UNKNOWN;
 }
 
 void healthd_board_init(struct healthd_config *config)
 {
-    config->batteryChargeTypePath  = "/sys/class/power_supply/battery/status";
+    config->batteryChargeRatePath  = "/sys/class/power_supply/battery/status";
     config->mapBatteryStatusString = mapBatteryStatusString;
-    config->mapChargeTypeString    = mapChargeTypeString;
+    config->mapChargeRateString    = mapChargeRateString;
 }
 
 
