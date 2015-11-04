514067e6f039b9dbb122556e9d6ffd82efc58f4e
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-04 12:52:56 -0500

    mofd-common: Add a shim library for icu version 53 compatibility
    Change-Id: Ifcd8b85c6296fab9fa6d112bf98112ff15cb443c

---
+++ libshim_icu53/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := icu53.c
+LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libshim_icu53
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
