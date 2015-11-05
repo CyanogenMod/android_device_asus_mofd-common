bdf153326bc58c9e527a8ce1aa1721b4ecbaf926
Jason Riordan <jriordan001@gmail.com>
2015-11-04 20:42:32 -0500

    mofd-common: shim-camera: Add more missing symbols
    Change-Id: I958464c8d34fd34f3815cc8998220f8933b65962

--- libshim_camera/Android.mk
+++ libshim_camera/Android.mk
@@ -2,10 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils
+LOCAL_SHARED_LIBRARIES := liblog libcutils libui libgui libbinder libutils
 
 LOCAL_SRC_FILES := \
-    mofd_camera.c
+    graphic-buffer.cpp \
+    mofd_camera.c \
+    surface-control.cpp \
 
 LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
