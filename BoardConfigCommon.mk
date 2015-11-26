55a8f967090436889fa014cbe8b2caacdb58e651
Christopher R. Palmer <crpalmer@gmail.com>
2015-11-26 08:52:27 -0500

    mofd-common: init: Fix a pile of warnings during boot
    Change-Id: Ib8acc49d0b8542c65616f4c8dfa57647b979f5c8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -90,17 +90,7 @@ SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
 BOARD_EGL_CFG := device/asus/mofd-common/configs/egl.cfg
 
 ADDITIONAL_DEFAULT_PROPERTIES += \
-    ro.opengles.version = 196608 \
-    ro.hwui.drop_shadow_cache_size = 6 \
-    ro.hwui.gradient_cache_size = 1 \
-    ro.hwui.layer_cache_size = 48 \
-    ro.hwui.path_cache_size = 32 \
-    ro.hwui.text_large_cache_width = 2048 \
-    ro.hwui.text_large_cache_height = 1024 \
-    ro.hwui.text_small_cache_width = 1024 \
-    ro.hwui.text_small_cache_height = 1024 \
-    ro.hwui.texture_cache_flushrate = 0.4 \
-    ro.hwui.texture_cache_size = 72 \
+    ro.opengles.version = 196608
 
 MAX_EGL_CACHE_ENTRY_SIZE := 65536
 MAX_EGL_CACHE_SIZE := 1048576
