06de248a2baceca204d049c8fb773f042172ef76
Christopher R. Palmer <crpalmer@gmail.com>
2015-12-16 16:25:54 -0500

    WIP: mofd-common SELinux bringup
    This currently has no undefined domains on boot, but doesn't
    even come close to booting without audit denials
    
    Change-Id: Ie6c78514f6e70ca5c78911a4ee6efd116fbce74d

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -213,7 +213,7 @@ LOCAL_MODULE       := rfkill_bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/rfkill_bt.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
