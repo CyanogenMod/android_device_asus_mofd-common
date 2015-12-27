38b80ffc47b96f0ae3cbce0c7b59b53b647302a6
Jason Riordan <jriordan001@gmail.com>
2015-12-26 19:10:00 -0500

    [DO NOT MERGE] mofd:force zram on all varients
    Change-Id: Ic599c7bd6d663e87898c1faf92632d8b3ee0b875

--- rootdir/etc/init.zram.sh
+++ rootdir/etc/init.zram.sh
@@ -5,7 +5,7 @@ MemTotal=${MemTotalStr:16:8}
 ZRAM_THRESHOLD=3000000
 IsLowMemory=0
 ((IsLowMemory=MemTotal<ZRAM_THRESHOLD?1:0))
-if [ $IsLowMemory -eq 1 ]; then
+#if [ $IsLowMemory -eq 1 ]; then
     setprop ro.config.zram true
-fi
+#fi
 
