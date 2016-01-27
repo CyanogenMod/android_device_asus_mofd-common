3ffb171f3f9e80f793eac66b802f5bfa02584b8b
Christopher R. Palmer <crpalmer@gmail.com>
2016-01-27 15:14:13 -0500

    mofd-common: Remove interactive governor from PowerHAL
    Change-Id: Ic3df4b1a30966507f2e7cf652db85bcb7e6862ee

--- power/power.cpp
+++ power/power.cpp
@@ -16,59 +16,17 @@
 
 #include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-
-#include <utils/SystemClock.h>
+#include <hardware/power.h>
 
 #define LOG_TAG "PowerHAL"
 #include <utils/Log.h>
-
-#include <hardware/hardware.h>
-#include <hardware/power.h>
 
 struct local_power_module {
     struct power_module base;
 };
 
 #define BUF_SIZE 80
-
-#define HISPEED_FREQ_PATH "/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq"
-#define HISPEED_FREQ_OFF  "500000"
-#define TOUCHBOOST_PATH   "/sys/devices/system/cpu/cpufreq/interactive/touchboostpulse"
-#define TOUCHBOOST_MIN_INTERVAL_MS 200
-
-static int64_t last_touchboost;
-static char hispeed_freq[BUF_SIZE];
-
-static int sysfs_read(const char *path, char buf[BUF_SIZE]) {
-    int len;
-    int fd = open(path, O_RDONLY);
-
-    if (fd < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error opening %s: %s\n", path, buf);
-        return fd;
-    }
-
-    len = read(fd, buf, BUF_SIZE-1);
-    if (len < 0) {
-        strerror_r(errno, buf, sizeof(buf));
-        ALOGE("Error read from %s: %s\n", path, buf);
-    } else {
-        buf[len] = '\0';
-    }
-
-    close(fd);
-    return len;
-}
-
-static void sysfs_read_or_empty(const char *path, char buf[BUF_SIZE]) {
-    if (sysfs_read(path, buf) < 0) {
-         buf[0] = '\0';
-    }
-}
 
 static void sysfs_write(const char *path, const char *const s) {
     char buf[BUF_SIZE];
@@ -90,53 +48,13 @@ static void sysfs_write(const char *path, const char *const s) {
     close(fd);
 }
 
-static void sysfs_write_unless_empty(const char *path, const char *const s) {
-    if (*s) {
-        sysfs_write(path, s);
-    }
+static void power_init(struct power_module *) {
 }
 
-void power_set_interactive(struct power_module *, int on) {
-    if (on) {
-        sysfs_write_unless_empty(HISPEED_FREQ_PATH, hispeed_freq);
-    } else {
-        sysfs_read_or_empty(HISPEED_FREQ_PATH, hispeed_freq);
-        sysfs_write(HISPEED_FREQ_PATH, HISPEED_FREQ_OFF);
-    }
+static void power_set_interactive(struct power_module *, int on __unused) {
 }
 
-static void touchboost() {
-    int64_t now = android::uptimeMillis();
-    if (now - last_touchboost > TOUCHBOOST_MIN_INTERVAL_MS) {
-        sysfs_write(TOUCHBOOST_PATH, "1");
-        last_touchboost = now;
-    }
-}
-
-static void power_hint(struct power_module *module, power_hint_t hint, void *data)
-{
-    struct local_power_module *pm = (struct local_power_module *) module;
-    int duration;
-
-    switch (hint) {
-    case POWER_HINT_INTERACTION:
-        touchboost();
-        break;
-
-    case POWER_HINT_CPU_BOOST:
-        duration = data != NULL ? (int) data : 1;
-        touchboost();
-
-    case POWER_HINT_VSYNC:
-        break;
-
-    default:
-        break;
-    }
-}
-
-static void power_init(struct power_module *)
-{
+static void power_hint(struct power_module *module __unused, power_hint_t hint __unused, void *data __unused) {
 }
 
 static struct hw_module_methods_t power_module_methods = {
