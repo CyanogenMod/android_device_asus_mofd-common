22b72009fa46fa36d0705e39563583e9ee8eff69
blkredstarV <blkredstar5@gmail.com>
2016-01-30 14:16:01 -0600

    Revert "Use ScreenType util from cmsdk"
    This reverts commit 39e095213743738d087bcd66d0f65be34ef34715.

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -16,10 +16,9 @@
 
 package com.cyanogenmod.settings.device;
 
+import com.android.internal.util.cm.ScreenType;
+
 import android.app.ActionBar;
-
-import org.cyanogenmod.internal.util.ScreenType;
-
 import android.os.Bundle;
 import android.provider.Settings;
 import android.preference.PreferenceActivity;
