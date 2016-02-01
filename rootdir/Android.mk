a1129066fc4dad9cc4dfc9aaae80b832892eb50d
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-01 17:22:45 -0500

    mofd-common: Move intel_prop to /system/bin
    Change-Id: Ibff87d9161dd0169c9883f2ff47ae88e7a8b387c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -209,14 +209,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := intel_prop
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := sbin/intel_prop
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := thermald
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
