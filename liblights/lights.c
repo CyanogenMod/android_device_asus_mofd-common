affd61caa58d2f4f79b079d73c851c222eb3336b
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-06 07:01:08 -0500

    mofd-common: lights: Simplify the brightness computation
    Frameworks converts the brightness (0xXY in the range 0x00-0xff)
    to a colour by making it 0xffXYXYXY.
    
    The previous code essentially does (77*x+150*x+29*x)/256 where
    x is the original brightness.
    
    That is equivalent to just doing brightness & 0xff...
    
    Change-Id: I35e17da32c372c102820fe3e40d1b6a500a50373

--- liblights/lights.c
+++ liblights/lights.c
@@ -117,19 +117,11 @@ is_lit(struct light_state_t const* state)
 }
 
 static int
-rgb_to_brightness(struct light_state_t const* state)
-{
-    int color = state->color & 0x00ffffff;
-    return ((77*((color>>16)&0x00ff))
-            + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
-}
-
-static int
 set_light_backlight(__attribute__((unused)) struct light_device_t* dev,
         struct light_state_t const* state)
 {
     int err = 0;
-    int brightness = rgb_to_brightness(state);
+    int brightness = (state->color & 0xff);
     if(!dev) {
         return -1;
     }
