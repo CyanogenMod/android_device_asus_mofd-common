02a57434fe5cfa66c4a7223a9764100beb71d185
Danny Baumann <dannybaumann@web.de>
2016-06-14 14:35:57 +0200

    mofd-common: Refactor CMActions app
    Change-Id: I3b019b29fc12e816bbcb8bc7beda308229b5f1dd

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -17,7 +17,10 @@
 package com.cyanogenmod.settings.device;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
@@ -36,9 +39,20 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
     private static final String KEY_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
     private static final String KEY_PROXIMITY_WAKE = "proximity_wake_enable";
 
+    private Context mContext;
+    private Handler mGestureHandler = new Handler();
+
+    private SwitchPreference mAmbientDisplayPreference;
     private SwitchPreference mHandwavePreference;
     private SwitchPreference mProximityWakePreference;
     private SwitchPreference mHapticFeedback;
+
+    private SwitchPreference mCGesturePreference;
+    private SwitchPreference mEGesturePreference;
+    private SwitchPreference mSGesturePreference;
+    private SwitchPreference mVGesturePreference;
+    private SwitchPreference mWGesturePreference;
+    private SwitchPreference mZGesturePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,18 +68,36 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
         mProximityWakePreference.setOnPreferenceChangeListener(mGesturePrefListener);
         mHapticFeedback = (SwitchPreference) findPreference(KEY_HAPTIC_FEEDBACK);
         mHapticFeedback.setOnPreferenceChangeListener(mHapticPrefListener);
+        mCGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_C_GESTURE_KEY);
+        mCGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
+        mEGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_E_GESTURE_KEY);
+        mEGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
+        mSGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_S_GESTURE_KEY);
+        mSGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
+        mVGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_V_GESTURE_KEY);
+        mVGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
+        mWGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_W_GESTURE_KEY);
+        mWGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
+        mZGesturePreference =
+                (SwitchPreference) findPreference(CMActionsSettings.TOUCHSCREEN_Z_GESTURE_KEY);
+        mZGesturePreference.setOnPreferenceChangeListener(mGesturePrefListener);
 
         final ActionBar actionBar = getActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        mContext = getApplicationContext();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         mHapticFeedback.setChecked(CMSettings.System.getInt(getContentResolver(),
                 CMSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0);
-
         // If running on a phone, remove padding around the listview
         if (!ScreenType.isTablet(this)) {
             getListView().setPadding(0, 0, 0, 0);
@@ -114,6 +146,26 @@ public class TouchscreenGestureSettings extends PreferenceActivity {
                 return true;
             }
             return false;
+        }
+    };
+
+    private Preference.OnPreferenceChangeListener mGesturePrefListener =
+        new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                mGestureHandler.postDelayed(mUpdateGestures, 500);
+                return true;
+            }
+        };
+
+    private final Runnable mUpdateGestures = new Runnable() {
+        public void run(){
+            try {
+                CMActionsSettings.updateGestureMode(mContext);
+            }
+            catch (Exception e) {
+                // Can't do much anyway
+            }
         }
     };
 }
