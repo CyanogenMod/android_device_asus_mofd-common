fb4b0464154380a2adb1dbf85a72d5b496af1416
Jason Riordan <jriordan001@gmail.com>
2016-06-29 22:58:00 -0400

    mofd: ramdisk updates for BT/Wifi/NFC from M beta
    Change-Id: I5d361e53566261bc20c52201d6a78a36f967b5e8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := brcm_config_init.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/brcm_config_init.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := config_init.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
