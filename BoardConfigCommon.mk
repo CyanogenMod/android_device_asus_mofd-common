5e0d788ef4e47f24fb4cf00f494da11a0c9693f0
Jason Riordan <jriordan001@gmail.com>
2016-08-15 17:20:02 -0400

    mofd: charger: remove unused SHOW_PERCENTAGE flag
    Change-Id: Idea2443731205fcdea81f14f0d68f45dc987fd09

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,7 +58,6 @@ TARGET_PROVIDES_CAMERA_HAL := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGER_SHOW_PERCENTAGE := true
 
 # Dex-preoptimization: Speeds up initial boot (if we ever o a user build, which we don't)
 ifeq ($(HOST_OS),linux)
