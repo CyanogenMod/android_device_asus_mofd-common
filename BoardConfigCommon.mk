dac22789431cc1a63bc78cf39f76bf627535960c
jrior001 <jriordan001@gmail.com>
2016-09-03 12:21:59 -0400

    [DO NOT MERGE]: temp N hacks
    Change-Id: I80ac29051cc1901f497230b5beff24d9267a27ee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -72,7 +72,7 @@ endif
 BOARD_HARDWARE_CLASS := device/asus/mofd-common/cmhw
 
 # Healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.moorefield
+#BOARD_HAL_STATIC_LIBRARIES := libhealthd.moorefield
 
 # Houdini: enable ARM codegen for x86
 BUILD_ARM_FOR_X86 := true
@@ -184,7 +184,7 @@ BUILD_WITH_SECURITY_FRAMEWORK := chaabi_token
 BUILD_WITH_CHAABI_SUPPORT := true
 
 # SELinux
-BOARD_SEPOLICY_DIRS += device/asus/mofd-common/sepolicy
+#BOARD_SEPOLICY_DIRS += device/asus/mofd-common/sepolicy
 
 # Tap-to-Wake
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/pci0000:00/0000:00:09.2/i2c-7/7-0038/ftsdclickmode"
