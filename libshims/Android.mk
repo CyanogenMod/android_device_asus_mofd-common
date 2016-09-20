e6199ff5e7c6e3fbd025fe1a9a780939da3cfc90
jrior001 <jriordan001@gmail.com>
2016-09-19 21:59:06 -0400

    mofd: shims: revive camera shim for LP cam blobs
    Change-Id: I19fdd8a7de4856bf76bf6e23a6116809b99ee7fb

---
+++ libshims/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+# Camera
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    gui/SensorManager.cpp \
+    ui/GraphicBufferAllocator.cpp \
+    ui/GraphicBuffer.cpp \
+    ui/GraphicBufferMapper.cpp \
+    surface-control.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
+LOCAL_MODULE := libshim_camera
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
